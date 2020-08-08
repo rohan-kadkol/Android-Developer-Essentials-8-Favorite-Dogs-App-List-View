c8d781ca7777a2184a930f95387ff32853e00a9f
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-08-08 10:57:47 -0500

    Added the TODOs for step3

--- app/src/main/java/com/rohankadkol/favoritedogsapplistview8/MainActivity.java
+++ app/src/main/java/com/rohankadkol/favoritedogsapplistview8/MainActivity.java
@@ -20,4 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1): Add a package called adapters
     // TODO (2): Create a class called DogsAdapter
+    // TODO (3): Create two global variables. Context mContext and Dog[] mDogs
+    // TODO (4): Create a constructor to save the passed context to a global Context variable called mContext
+    // TODO (5): Override getView()
+    // TODO (6): Configure getView() to return the view populated with the correct dog information
+    // TODO (7): Override getCount()
+    // TODO (8): Configure getCount() to return mDogs.length
 }
