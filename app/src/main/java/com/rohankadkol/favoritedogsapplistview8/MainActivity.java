2c90dd29bb2c0d522f1ba20469a5ac53c986bfec
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-08-08 11:13:11 -0500

    Solutions added for Step4

--- app/src/main/java/com/rohankadkol/favoritedogsapplistview8/MainActivity.java
+++ app/src/main/java/com/rohankadkol/favoritedogsapplistview8/MainActivity.java
@@ -23,6 +23,4 @@ public class MainActivity extends AppCompatActivity {
     // TODO (3): Have an ImageView (iv_dog) with weight 4 and a ScrollView with weight 6
     // TODO (4): Within the ScrollView, add a vertical LinearLayout
     // TODO (5): Then add 6 TextViews in the following order: Name, Breed, Likes Label, Likes, Dislikes Label, Dislikes
-    // TODO (6): Then add 6 TextViews in the following order: Name, Breed, Likes Label, Likes, Dislikes Label, Dislikes
-
 }
