79a03cfb889064d29e2a4d48519cf6043f02943e
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-08-08 10:44:22 -0500

    Removed old TODOs

--- app/src/main/java/com/rohankadkol/favoritedogsapplistview8/MainActivity.java
+++ app/src/main/java/com/rohankadkol/favoritedogsapplistview8/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.ImageView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    // TODO (4): Create a global variable for the ListView
     private ListView mLvDogs;
 
     @Override
@@ -16,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (5): use findViewById to initialize the above global variable
         mLvDogs = findViewById(R.id.lv_dogs);
     }
 }
